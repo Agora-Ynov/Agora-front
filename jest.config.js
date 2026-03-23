d36fa9547b963d0ebc43504703c649b7351ffd71
amine-cheraiti <amine-cheraitia@hotmail.com>
2026-03-23 16:18:00 +0100

    chore(ci): correction pipeline - ESLint + Prettier + Jest + scripts npm

---
+++ jest.config.js
@@ -0,0 +1,37 @@
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterFramework: ['<rootDir>/setup-jest.ts'],
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  testPathPattern: 'src/.*\\.spec\\.ts$',
+  testMatch: ['**/src/**/*.spec.ts'],
+  collectCoverageFrom: [
+    'src/app/**/*.ts',
+    '!src/app/**/*.model.ts',
+    '!src/app/**/*.module.ts',
+    '!src/main.ts',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 60,
+      functions: 70,
+      lines: 70,
+    },
+  },
+  coverageReporters: ['html', 'lcov', 'json-summary', 'text'],
+  transform: {
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
+  },
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
+  moduleNameMapper: {
+    '^@environments/(.*)$': '<rootDir>/src/environments/$1',
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+  },
+};
