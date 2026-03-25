a9f0d6973979f5504986a37a873986adb90e0229
amine-cheraiti <amine-cheraitia@hotmail.com>
2026-03-25 11:58:29 +0100

    chore(app): enable minimal local catalogue routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'catalogue',
+  },
+  {
+    path: 'catalogue',
+    loadComponent: () =>
+      import('./features/reservation/catalogue/catalogue.component').then(
+        m => m.CatalogueComponent
+      ),
+  },
+  {
+    path: 'login',
+    redirectTo: 'catalogue',
+  },
+  {
+    path: 'register',
+    redirectTo: 'catalogue',
+  },
+  {
+    path: 'calendar',
+    redirectTo: 'catalogue',
+  },
+  {
+    path: '**',
+    redirectTo: 'catalogue',
+  },
+];
